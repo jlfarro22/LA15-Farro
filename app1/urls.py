f374d059699c2b1650e8414f0a87f75f75047dde
jlfarro22 <johnlynard.farro7@gmail.com>
2024-12-14 14:28:53 +0800

    first commit

---
+++ app1/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('hello/', views.say_hello),
+    path('hi/', views.say_hi),
+    path('posts/', views.blog_list),
+    path('posts/<id>/', views.blog_detail)
+]
